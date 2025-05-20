312c73e85c33d54d30089cb00845714c370bf85a
qwertydreey <andreitan598@gmail.com>
2025-05-20 09:50:14 +0800

    Revert "Update app.py"
    This reverts commit 23afc9503a7c34e1a25b3c6a783d80c7476d92ff.

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ bcrypt = Bcrypt(app)
 
 # Connect to MySQL
 db = mysql.connector.connect(
-    host="172.19.112.1",
+    host="host.docker.internal",
     user="root",
     password="qweqwe",
     database="learning_game"
